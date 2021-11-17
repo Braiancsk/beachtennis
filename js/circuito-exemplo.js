cb4db65bf6a49e626c3d2e36c92f4dd4f3774cb2
Braian <braianfelipecsk123@hotmail.com>
2021-11-17 18:06:05 -0300

    Função de slide nas tabs

---
+++ js/circuito-exemplo.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: "auto",
+    centeredSlides: false,
+    spaceBetween: 10,
+    freeMode: true,
+        breakpoints: {
+            992: {
+              slidesPerView: 4,
+              spaceBetween: 0,
+            },    
+          },
+  });
+
+
+    
+
+
